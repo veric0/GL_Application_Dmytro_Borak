ab9daf70fa99b2f920c6bb128ba5362955194f21
Dmytro Borak <borak.dmytro@gmail.com>
2024-02-29 19:43:23 +0200

    add user input

--- application.cpp
+++ application.cpp
@@ -1,22 +1,60 @@
 #include <iostream>
 #include "multithreaded_file_search.h"
 
-// example of use searchForFile
-int main() {
+/**
+ * @brief Requests the user to enter the folder path and the file name to search for.
+ *
+ * @param[out] folderPath The variable to store folder path.
+ * @param[out] fileName The variable to store file name for search.
+ * @return true if the input is correct;
+ *         false if the input was incorrect or the user canceled the input.
+ */
+bool getInput(std::string &folderPath, std::string &fileName) {
+    std::cout << "Enter the folder path: ";
+    std::getline(std::cin, folderPath);
+    if (!fs::is_directory(folderPath)) {
+        std::cerr << "Error: Invalid folder path." << std::endl;
+        return false;
+    }
 
+    std::cout << "Enter the file name to search for: ";
+    std::getline(std::cin, fileName);
+    if (fileName.empty()) {
+        std::cerr << "Error: File name cannot be empty." << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * example of usage searchForFile
+ * @return
+ */
+int main() {
+    // Number of threads available
     unsigned int cores = std::thread::hardware_concurrency();
     std::cout << "Number of cores ( =threads for search): " << cores << std::endl;
 
-//    std::string directoryPath = "/";
-    std::string directoryPath = "/home/dima/CLionProjects";
-    std::string fileName = "GL_Application_Dmytro_Borak";
+    std::string directoryPath;
+    std::string fileName;
+
+    // Input path and filename or use default names
+    if (!getInput(directoryPath, fileName)) {
+        directoryPath = "/home";
+//    directoryPath = "/home/dima/CLionProjects";
+        fileName = "GL_Application_Dmytro_Borak";
+    }
+
+    std::cout << "Directory path: " << directoryPath << std::endl;
+    std::cout << "File name: " << fileName << std::endl;
 
     std::vector<fs::path> foundPaths;
     searchForFile(fileName, directoryPath, foundPaths);
 
     if (!foundPaths.empty()) {
         std::cout << "Found " << foundPaths.size() << " occurrences of " << fileName << ":\n";
-        for (const auto& path : foundPaths) {
+        for (const auto &path: foundPaths) {
             std::cout << path << '\n';
         }
     } else {
